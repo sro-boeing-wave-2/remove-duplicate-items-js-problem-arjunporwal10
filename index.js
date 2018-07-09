5268bc1295b924ce2d4b8ab8ab9276a5c55cc1c3
arjun <arjunporwal10@gmail.com>
2018-07-09 15:39:35 +0530

    test case passed

--- index.js
+++ index.js
@@ -3,8 +3,15 @@
  * @param {Array} items
  * @returns {Array}
  */
-const removeDuplicateItems = (items) => 
-
+const removeDuplicateItems = (items) => {
+    var len = items.length;
+    var mySet = new Set();
+    for (var i = 0; i<len; i++)
+    {
+        mySet.add(items[i]);
+    }
+    var myArr = Array.from(mySet);
+    return myArr;
 };
 
 module.exports = removeDuplicateItems;
