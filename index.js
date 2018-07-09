6f6a85f824ada3279988013c296f034aaf383f4e
arjun <arjunporwal10@gmail.com>
2018-07-09 15:57:32 +0530

    linting passed

--- index.js
+++ index.js
@@ -4,14 +4,13 @@
  * @returns {Array}
  */
 const removeDuplicateItems = (items) => {
-    var len = items.length;
-    var mySet = new Set();
-    for (var i = 0; i<len; i++)
-    {
-        mySet.add(items[i]);
-    }
-    var myArr = Array.from(mySet);
-    return myArr;
+  const len = items.length;
+  const mySet = new Set();
+  for (let i = 0; i < len; i += 1) {
+    mySet.add(items[i]);
+  }
+  const myArr = Array.from(mySet);
+  return myArr;
 };
 
 module.exports = removeDuplicateItems;
