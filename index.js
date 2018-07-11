2f4ace5ef7b65a08970c723a37cc155c63006e0c
arjun <arjunporwal10@gmail.com>
2018-07-11 10:12:55 +0530

    issues resolved

--- index.js
+++ index.js
@@ -3,14 +3,5 @@
  * @param {Array} items
  * @returns {Array}
  */
-const removeDuplicateItems = (items) => {
-  const len = items.length;
-  const mySet = new Set();
-  for (let i = 0; i < len; i += 1) {
-    mySet.add(items[i]);
-  }
-  const myArr = Array.from(mySet);
-  return myArr;
-};
-
+const removeDuplicateItems = items => Array.from(new Set(items));
 module.exports = removeDuplicateItems;
